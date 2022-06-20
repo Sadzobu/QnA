785538c9ea947ec532b79942fa1836de6a65b9c3
Sadzobu <kaonokage@gmail.com>
2022-06-20 10:03:08 -0700

    deleted show action from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :questions, only: %i[new create show] do
-    resources :answers, only: %i[new create show]
+  resources :questions, only: %i[new create] do
+    resources :answers, only: %i[new create]
   end
 end
