4aaf37260ba3e5a7bdaad4fe8a7b10a8234e2114
Sadzobu <kaonokage@gmail.com>
2022-06-20 17:40:42 +0300

    added specs for Question controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,17 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
-  describe 'GET #index' do
-    let(:questions) { create_list(:question, 3) }
+  let(:question) { create(:question) }
 
-    before { get :index }
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new question to database' do
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
 
-    it 'populates an array of all questions' do
-      expect(assigns(:questions)).to match_array(questions)
+      it 'redirects to show' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to assigns(:question)
+      end
+
     end
-
-    it 'renders index view' do
-      expect(response).to render_template :index
+    context 'with invalid attributes' do
+      it 'does not save the question' do
+        expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
+      end
+      it 're-renders new view' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template :new
+      end
     end
   end
+
 end
