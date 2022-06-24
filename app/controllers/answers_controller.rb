a4e1c6473b5a7e7ca6f8ed7f88605e7c3b6ea7a4
Sadzobu <kaonokage@gmail.com>
2022-06-24 12:31:28 +0300

    added feature for User can see a list of Questions and User can answer question + refactoring

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,21 +1,12 @@
 class AnswersController < ApplicationController
-  before_action :load_question, only: %i[new create]
-
-  def new
-    @answer = @question.answers.new
-  end
 
   def create
-    @answer = @question.answers.new(answer_params)
-
-    render :new unless @answer.save
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.create(answer_params)
+    redirect_to @question
   end
 
   private
-
-  def load_question
-    @question = Question.find(params[:question_id])
-  end
 
   def answer_params
     params.require(:answer).permit(:body)
---
+++ spec/features/answers/create_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'User can answer question', %q{
+  In order to quickly answer someone's question
+  As an authenticated user
+  I'd like to be able to write an answer on question's page
+} do
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+
+  describe 'Authenticated user' do
+    scenario 'answers a question' do
+      sign_in(user)
+
+      visit question_path(question)
+      
+      fill_in 'answer_body', with: 'Test answer'
+      click_on 'Answer'
+
+      expect(page).to have_content 'Test answer'
+    end
+
+    scenario 'answers a question with errors'
+  end
+
+  scenario 'Unauthenticated user tries to answer a question'
+
+end
