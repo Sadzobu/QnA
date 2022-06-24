f409ace5fae191a3f29969af299110675a5725f3
Sadzobu <kaonokage@gmail.com>
2022-06-24 14:23:53 +0300

    added story User can log out

---
+++ spec/features/log_out_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'user can log out', %q{
+  In order to finish working with application
+  As an authenticated user
+  I'd like to be able to log out
+} do
+
+  given(:user) { create(:user) }
+
+  scenario 'Authenticated user tries to log out' do
+    sign_in(user)
+    visit questions_path
+    click_on 'Log out'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
