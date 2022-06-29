be216d4c62757bb328ce77bd106d729991a157ec
Sadzobu <kaonokage@gmail.com>
2022-06-29 10:43:13 -0700

    add barebone story User can mark answer on their question best and it shows first in answers list

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,24 +1,32 @@
 class AnswersController < ApplicationController
   before_action :authenticate_user!
   before_action :load_question, only: %i[create]
+  before_action :load_answer, only: %i[destroy update mark_as_best]
   
   def create
     @answer = @question.answers.create(answer_params)
   end
 
   def destroy
-    @answer = Answer.find(params[:id])
     @question = @answer.question
     @answer.destroy if current_user.author_of?(@answer)
   end
 
   def update
-    @answer = Answer.find(params[:id])
     @answer.update(answer_params)
     @question = @answer.question
   end
 
+  def mark_as_best
+    @question = @answer.question
+    @question.update(best_answer_id: @answer.id)
+  end
+
   private
+
+  def load_answer
+    @answer = Answer.find(params[:id])
+  end
 
   def load_question
     @question = Question.find(params[:question_id])
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,15 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: 'User'
+  belongs_to :best_answer, class_name: 'Answer', optional: true
 
   validates :title, :body, presence: true
+
+  def best_answer?
+    best_answer != nil
+  end
+
+  def other_answers
+    answers.where.not(id: best_answer_id)
+  end
 end
