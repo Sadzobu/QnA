9e9cc4c7c6f48b8754470cd851a9beefe2e9b2fc
Sadzobu <kaonokage@gmail.com>
2022-06-30 11:28:41 +0300

    completed story User can mark answer as best

--- spec/features/answers/mark_as_best_spec.rb
+++ spec/features/answers/mark_as_best_spec.rb
@@ -5,14 +5,73 @@ feature 'User can mark answer as best on their question', %q{
   As an authenticated user and author of the question
   I'd like to be able to mark answer as best
 } do
-  describe 'Authenticated user' do
-      scenario 'marks answer best on their question'
 
-      scenario 'tries to mark answer best on other users question'
+  given(:user) { create(:user) }
+  given(:question) { create(:question, author: user) }
+  given(:other_question) { create(:question) }
+  given!(:answers) { create_list(:answer, 3, question: question) }
+
+  describe 'Authenticated user' do
+    background { sign_in(user) }
+
+    scenario 'marks answer best on their question', js: true do
+      visit question_path(question)
+      new_best_answer = answers[2]
+
+      within('.answers') do
+        within("#answer_#{new_best_answer.id}") do
+          click_on 'Best'
+        end
+
+        expect(current_path).to eq question_path(question)
+        expect(page).to have_css '.best_answer'
+      end
+    end
+
+    scenario 'tries to mark answer best on other users question' do
+      visit question_path(other_question)
+
+      expect(page).to_not have_link('Best')
+    end
   end
 
-  scenario 'Unauthenticated user tries to mark answer best'
+  scenario 'Unauthenticated user tries to mark answer best' do
+    visit question_path(question)
 
-  scenario 'Best answer should be first in answers block'
+    expect(page).to_not have_link('Best')
+  end
+
+  scenario 'There could be only one best answer', js: true do
+    sign_in(user)
+    visit question_path(question)
+    old_best_answer = answers[2]
+    new_best_answer = answers[0]
+
+    within('.answers') do
+      within("#answer_#{old_best_answer.id}") do
+        click_on 'Best'
+      end
+    
+      within("#answer_#{new_best_answer.id}") do
+        click_on 'Best'
+      end
+
+      expect(page).to_not have_css('.best_answer').twice
+    end
+  end
+
+  scenario 'Best answer should be first in answers', js: true do
+    sign_in(user)
+    visit question_path(question)
+    new_best_answer = answers[2]
+
+    within('.answers') do
+      within("#answer_#{new_best_answer.id}") do
+        click_on 'Best'
+      end
+    end
+
+    expect(find('.answers', match: :first)).to have_css '.best_answer'
+  end
 
 end
