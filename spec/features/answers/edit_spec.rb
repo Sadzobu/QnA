604d78f9de1bcc3ca9fe2903ea290f9ef2c709e2
Sadzobu <kaonokage@gmail.com>
2022-07-04 15:40:40 +0300

    added story User can add multiple files while editing answer

--- spec/features/answers/edit_spec.rb
+++ spec/features/answers/edit_spec.rb
@@ -12,14 +12,15 @@ feature 'User can edit their answer', %q{
   given!(:other_answer) { create(:answer, body: "Other user answer", question: question) }
 
   describe 'Authenticated user' do
-    background { sign_in(user) }
-
-    scenario 'edits their answer', js: true do
+    background do
+      sign_in(user)
       visit question_path(question)
-      click_on 'Edit'
-      expect(current_path).to eq question_path(question)
+    end
 
-      within '.answers' do
+    scenario 'edits their answer without loading any new pages', js: true do
+      within "#answer_#{answer.id}" do
+        click_on 'Edit'
+        expect(current_path).to eq question_path(question)
         fill_in 'Your answer', with: 'New body'
         click_on 'Save'
         expect(current_path).to eq question_path(question)
@@ -31,28 +32,32 @@ feature 'User can edit their answer', %q{
     end
 
     scenario 'tries to edit their answer with errors', js: true do
-      visit question_path(question)
-
       within "#answer_#{answer.id}" do
         click_on 'Edit'
-        expect(current_path).to eq question_path(question)
 
         fill_in 'Your answer', with: ''
         click_on 'Save'
-        expect(current_path).to eq question_path(question)
       end
-      
       expect(page).to have_content "Body can't be blank"
     end
 
     scenario "tries to edit other user's answer" do
-      visit question_path(question)
-
       within "#answer_#{other_answer.id}" do
         expect(page).to_not have_selector 'edit-answer-link'
       end
     end
 
+    scenario 'adds files while editing answer', js: true do
+      within "#answer_#{answer.id}" do
+        click_on 'Edit'
+        fill_in 'Your answer', with: 'New body'
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
   end
 
   scenario 'Unauthenticated user tries to edit answer' do
