d2ce72924efd46ddd6e07a7fc49c38b7ad6b1a76
Sadzobu <kaonokage@gmail.com>
2022-07-04 14:01:48 +0300

    added active storage support, added story User can add multiple files when creating question

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,11 +3,9 @@ class Question < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :best_answer, class_name: 'Answer', optional: true
 
-  validates :title, :body, presence: true
+  has_many_attached :files
 
-  def best_answer?
-    best_answer != nil
-  end
+  validates :title, :body, presence: true
 
   def other_answers
     answers.where.not(id: best_answer_id)
