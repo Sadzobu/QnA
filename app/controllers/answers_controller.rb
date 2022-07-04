7b2bfe74cdef7e19302892c73a49d13c3292fdac
Sadzobu <kaonokage@gmail.com>
2022-07-04 18:03:06 +0300

    added functionality to delete attachments, added story User can delete attachments from their questions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -25,7 +25,7 @@ class AnswersController < ApplicationController
   private
 
   def load_answer
-    @answer = Answer.find(params[:id])
+    @answer = Answer.with_attached_files.find(params[:id])
   end
 
   def load_question
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,11 +1,20 @@
 FactoryBot.define do
   factory :question do
-    title { "MyString" }
-    body { "MyText" }
+    title { 'MyString' }
+    body { 'MyText' }
     association :author, factory: :user
 
     trait :invalid do
       title { nil }
+    end
+
+    trait :with_attachment do
+      after(:build) do |question|
+        question.files.attach(
+          io: File.open("#{Rails.root}/spec/rails_helper.rb"),
+          filename: 'test.rb'
+        )
+      end
     end
   end
 end
