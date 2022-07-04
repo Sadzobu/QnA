995527fd55ab73748b6305ca329464652c44d7e6
Sadzobu <kaonokage@gmail.com>
2022-07-04 18:03:06 +0300

    added functionality to delete attachments, added story User can delete attachments from their questions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'questions#index'
 
+  resources :attachments, only: %i[destroy]
+
   resources :questions, only: %i[index show new create destroy update] do
     resources :answers, shallow: true, only: %i[create destroy update] do
       patch :mark_as_best, on: :member
--- spec/features/question/delete_spec.rb
+++ spec/features/question/delete_spec.rb
@@ -9,15 +9,62 @@ feature 'User can delete their question', %q{
   given(:user) { create(:user) }
   given!(:question) { create(:question, author: user) }
 
-  background { sign_in(user) }
+  describe 'Authenticated user' do
+    background do
+      sign_in(user)
+      visit questions_path
+    end
 
-  scenario 'Authenticated user tries to delete their question' do
+    scenario 'tries to delete their question' do
+      click_on 'Delete'
 
-    visit questions_path
+      expect(page).to_not have_content question.title
+    end
 
-    click_on 'Delete'
-
-    expect(page).to_not have_content question.title
+    scenario "tries to delete other user's questions" do
+      within "#question_#{question.id}" do
+        expect(page).to_not have_selector 'delete-question-link'
+      end
+    end
   end
 
+  scenario 'Unauthenticated user tries to delete question' do
+    visit questions_path
+
+    expect(page).not_to have_link 'Delete'
+  end
+
+  describe 'Attachment features' do
+    given!(:question) { create(:question, :with_attachment, author: user) }
+    given!(:other_question) { create(:question, :with_attachment) }
+
+    scenario 'Authenticated user tries to delete attachment on their question without reloading', js: true do
+      sign_in(user)
+      visit question_path(question)
+
+      within "#attachment_#{question.files[0].id}" do
+        click_on 'Delete'
+        expect(current_path).to eq question_path(question)
+      end
+
+      expect(page).to_not have_link 'question.files[0].filename.to_s'
+    end
+
+    scenario "Authenticated user tries to delete attachment on other user's question" do
+      sign_in(user)
+      visit question_path(other_question)
+
+      within "#attachment_#{other_question.files[0].id}" do
+        expect(page).to_not have_link 'Delete'
+      end
+    end
+
+    scenario 'Unauthenticated user tries to delete attachment on question' do
+      visit question_path(question)
+
+      within "#attachment_#{question.files[0].id}" do
+        expect(page).to_not have_link 'Delete'
+      end
+    end
+  end
 end
