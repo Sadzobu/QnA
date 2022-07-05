59ee2292af86f7b82e5bacc38e70fccdb1d3e41d
Sadzobu <kaonokage@gmail.com>
2022-07-05 09:42:21 +0300

    added specs for attachment controller

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe AttachmentsController, type: :controller do
+  let(:user) { create(:user) }
 
+  describe 'DELETE #destroy' do
+    before { login(user) }
+
+    context 'author of resource tries to delete attachment' do
+      let!(:question) { create(:question, :with_attachment, author: user) }
+
+      it 'deletes attachment' do
+        expect { delete :destroy, params: { id: question.files[0] }, format: :js }.to change(question.files, :count).by(-1)
+      end
+    end
+
+    context "user tries to delete other user's resource" do
+      let(:other_question) { create(:question, :with_attachment) }
+
+      it 'does not delete attachment' do
+        expect { delete :destroy, params: { id: other_question.files[0] }, format: :js }.to change(other_question.files, :count).by(0)
+      end
+    end
+  end
 end
