de186d3c79bbbe35d5b5b7b60a2204997335342f
Sadzobu <kaonokage@gmail.com>
2022-07-07 15:23:47 +0300

    corrected spec mistakes

--- spec/features/question/edit_links_spec.rb
+++ spec/features/question/edit_links_spec.rb
@@ -12,7 +12,9 @@ feature 'User can edit links in their question', %q{
   given!(:other_question) { create(:question, :with_link) }
 
   describe 'Authenticated user' do
-    background { sign_in(user) }
+    background do
+      sign_in(user)
+    end
 
     scenario 'edits link on their question', js: true do
       visit questions_path
@@ -22,16 +24,37 @@ feature 'User can edit links in their question', %q{
         fill_in 'Link name', with: 'Bing'
         fill_in 'Url', with: bing_url
         click_on 'Save'
-        expect(page).to_not have_link 'Bing', href: bing_url
+      end
+
+      visit question_path(question)
+      within '.links' do
+        expect(page).to have_link 'Bing', href: bing_url
       end
     end
 
     scenario 'tries to edit link on other user question' do
-      visit questions_path
-
       within "#question_#{other_question.id}" do
         expect(page).to_not have_link 'Edit'
       end
+    end
+
+    scenario 'adds new link to their question', js: true do
+      visit questions_path
+
+      within "#question_#{question.id}" do
+        click_on 'Edit'
+        click_on 'Add link'
+        page.all(:fillable_field, 'Link name')[1].set('Bing')
+        page.all(:fillable_field, 'Url')[1].set(bing_url)
+        click_on 'Save'
+      end
+
+      visit question_path(question)
+      within '.links' do
+        expect(page).to have_link question.links[0].name, href: question.links[0].url
+        expect(page).to have_link 'Bing', href: bing_url
+      end
+      
     end
   end
 
