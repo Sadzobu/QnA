e092efd51061b09158858ac7f7e5d713de0d5ab1
Sadzobu <kaonokage@gmail.com>
2022-07-09 00:15:35 -0700

    added story Givinf best answer now rewards answer author with question reward

--- spec/factories/rewards.rb
+++ spec/factories/rewards.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :reward do
-    
+    title { 'MyReward' }
+    association :question
+
+    after :build do |reward|
+      reward.file.attach(
+        io: File.open("#{Rails.root}/spec/assets/reward.png"),
+        filename: 'reward.png'
+      )
+    end
   end
 end
