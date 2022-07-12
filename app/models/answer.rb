b6f91be4687812e328a22f1009c018d51db21ac5
Sadzobu <kaonokage@gmail.com>
2022-07-12 07:50:43 -0700

    refactoring DRY linkable concern

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,13 +1,11 @@
 class Answer < ApplicationRecord
   include Voteable
+  include Linkable
   
   belongs_to :question
   belongs_to :author, class_name: 'User'
-  has_many :links, dependent: :destroy, as: :linkable
 
   has_many_attached :files
-
-  accepts_nested_attributes_for :links, reject_if: :all_blank
 
   validates :body, presence: true
 
